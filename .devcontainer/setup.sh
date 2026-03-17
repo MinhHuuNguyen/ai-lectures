089212caf7fd3f47a8d992a6b836d936a299f7bc
daby <minhhuunguyen1511@gmail.com>
2026-03-17 22:43:25 +0700

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -7,9 +7,9 @@ export PATH="$HOME/.local/bin:$PATH"
 
 cd /workspaces/ai-lectures/venv/common_venv
 uv sync; uv lock
-source /workspaces/ai-lectures/venv/common_venv/bin/activate
+source /workspaces/ai-lectures/venv/common_venv/.venv/bin/activate
 cd /workspaces/ai-lectures/
 
-cp 4_deep_learning/notebook/3-generative-ai/.env.example 4_deep_learning/notebook/3-generative-ai/.env
-cp 4_deep_learning/notebook/4-ai-agent/.env.example 4_deep_learning/notebook/4-ai-agent/.env
-cp 4_deep_learning/notebook/9-retrieval-augmented-generation/.env.example 4_deep_learning/notebook/9-retrieval-augmented-generation/.env
+cp /workspaces/ai-lectures/4_deep_learning/notebook/3-generative-ai/.env.example /workspaces/ai-lectures/4_deep_learning/notebook/3-generative-ai/.env
+cp /workspaces/ai-lectures/4_deep_learning/notebook/4-ai-agent/.env.example /workspaces/ai-lectures/4_deep_learning/notebook/4-ai-agent/.env
+cp /workspaces/ai-lectures/4_deep_learning/notebook/9-retrieval-augmented-generation/.env.example /workspaces/ai-lectures/4_deep_learning/notebook/9-retrieval-augmented-generation/.env
